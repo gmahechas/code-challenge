df636e40a80a63b0184a46925ce56992c55cf187
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-09-29 21:28:02 -0500

    updates

---
+++ crack-the-coding/01-quick-short.js
@@ -0,0 +1,59 @@
+/* 
+
+						 start
+							 |
+array = [2,6,5,3,8,7,1,0]  1. choose a pivot (3)
+array = [2,6,5,0,8,7,1,3] 2. move pivot at the end
+
+[2,6,5,0,8,7,1,3]
+ |-----------|
+from left search item larger that pivot = 6
+from right sear item smaller that pivot = 1
+and swap
+result [2,1,5,0,8,7,6,3]
+
+from left search item larger that pivot = 5
+from right sear item smaller that pivot = 0
+and swap
+result [2,1,0,5,8,7,6,3]
+
+from left search item larger that pivot = 5 // stop when index of left > right
+from right sear item smaller that pivot = 0
+swap left item with our pivot
+result [2,1,0,3,8,7,6,5]
+
+
+----------------------------------------------
+right part [8,7,6,5]
+
+1. [8,7,6,5] pivot=7
+2. [8,5,6,7]
+
+from left search item larger that pivot = 8
+from right sear item smaller that pivot = 6
+
+result. [6,5,8,7]
+
+from left search item larger that pivot = 8
+from right sear item smaller that pivot = 5
+result. [6,5,7,8]
+
+do the same for left part
+
+----------------------------------------------
+
+How to choose a pivot
+
+ |       |       |
+[2,6,5,3,8,7,1,0,4]
+
+// swap
+        pivot
+ |       |       |
+[2,6,5,3,4,7,1,0,8]
+
+
+Worst = O(n2)
+Average = nLog(n)
+
+*/
