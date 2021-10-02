ef76bfd7ecfa4c2b57724788d4c03259973adad6
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-10-01 21:52:00 -0500

    max char

--- coding-interview-stephen/src/exercises/maxchar/index.js
+++ coding-interview-stephen/src/exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	const mapChar = {};
 
+	for (const letter of str) {
+		if(!mapChar[letter]) {
+			mapChar[letter] = 1
+		} else {
+			mapChar[letter]++;
+		}
+	}
+
+	let maxChar = 0;
+	let maxCharIndex = '';
+
+	for (const [index, value] of Object.entries(mapChar)) {
+		if(value > maxChar) {
+			maxChar = value;
+			maxCharIndex = index;
+		}
+	}
+	return maxCharIndex;
+}
+
+console.log(maxChar('abcccccccd'))
 module.exports = maxChar;
