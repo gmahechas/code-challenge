50445006fb440c148394b066614d66947748968c
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-10-14 22:58:31 -0500

    capitalize

--- coding-interview-stephen/src/exercises/anagrams/index.js
+++ coding-interview-stephen/src/exercises/anagrams/index.js
@@ -8,6 +8,52 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
 
-module.exports = anagrams;
+function cleanString(str) {
+	return str.replace(/[^\w]/g, "").toLowerCase();
+}
+
+function stringToMap(str) {
+
+	const mapString = {};
+
+	for (const letter of str) {
+		mapString[letter] = mapString[letter]++ || 1
+	}
+
+	return mapString;
+
+}
+
+function anagrams(stringA, stringB) { // solution 1
+
+	const stringACleaned = cleanString(stringA);
+	const stringAMapped = stringToMap(stringACleaned);
+	
+	const stringBCleaned = cleanString(stringB);
+	const stringBMapped = stringToMap(stringBCleaned);
+
+	if(Object.keys(stringAMapped).length !== Object.keys(stringBMapped).length) {
+		return false;
+	}
+
+	for (const charA in stringAMapped) {
+		if(stringAMapped[charA] !== stringBMapped[charA]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+/* ############### solution 2 */
+function cleanString2(str) {
+	return str.replace(/[^\w]/g, "").toLowerCase().split('').sort().join('');
+}
+
+function anagrams2(stringA, stringB) { // solution 2
+	return cleanString2(stringA) == cleanString2(stringB);
+}
+
+console.log(anagrams('rail safety', 'fairy tales'))
+module.exports = anagrams2; // test with solution 2
