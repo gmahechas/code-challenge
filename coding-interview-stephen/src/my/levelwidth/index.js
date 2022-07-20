08794c358daac76de10290ef88ec3edd95e84ac0
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-07-20 06:54:26 -0500

    binary

--- coding-interview-stephen/src/my/levelwidth/index.js
+++ coding-interview-stephen/src/my/levelwidth/index.js
@@ -5,12 +5,45 @@
 // --- Example
 // Given:
 //     0
-//   / |  \
+//  /  |  \
 // 1   2   3
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+const Node  = require('./node');
+
+function levelWidth(root) {
+	let arr = [root, 's'];
+	const counters = [0];
+
+	while(arr.length > 1) {
+		debugger;
+		const node = arr.shift();
+		debugger;
+		if(node === 's') {
+			debugger;
+			counters.push(0);
+			arr.push('s');
+			debugger;
+		} else {
+			debugger;
+			arr.push(...node.children);
+			counters[counters.length - 1]++;
+			debugger;
+		}
+	}
+
+	return counters;
+}
+
+const root = new Node(0);
+root.add(1);
+root.add(2);
+root.add(3);
+root.children[0].add(4);
+root.children[2].add(5);
+
+console.log(levelWidth(root));
 
 module.exports = levelWidth;
--- coding-interview-stephen/src/my/tree/index.js
+++ coding-interview-stephen/src/my/tree/index.js
@@ -10,8 +10,100 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.children = [];
+	}
 
-class Tree {}
+	add(data) {
+		const node = new Node(data);
+		this.children.push(node);
+		return node;
+	}
+
+	remove(data) {
+		this.children = this.children.flter(node => node.data !== data);
+	}
+}
+
+class Tree {
+	constructor() {
+		this.root = null;
+	}
+
+	traverseBF(fn) {
+		let arr = [this.root];
+		while(arr.length) {
+			const node = arr.shift();
+			arr.push(...node.children);
+			fn(node);
+		}
+	}
+
+	traverseBFFromNode(fn, node) {
+		let arr = [node];
+		while (arr.length) {
+			const node = arr.shift();
+			arr.push(...node.children);
+			fn(node);
+		}
+	}
+
+	traverseDF(fn) {
+		let arr = [this.root];
+		while (arr.length) {
+			const node = arr.shift();
+			arr.unshift(...node.children);
+			fn(node);
+		}
+	}
+
+	traverseDFFromNode(fn, node) {
+		let arr = [node];
+		while (arr.length) {
+			const node = arr.shift();
+			arr.unshift(...node.children);
+			fn(node);
+		}
+	}
+}
+
+const tree = new Tree();
+const node1 = new Node('1');
+
+/* level 1 */
+tree.root = node1;
+
+/* level 2 */
+const node2 = node1.add('2');
+const node3 = node1.add('3');
+
+/* level 3 */
+const node4 = node2.add('4');
+const node5 = node2.add('5');
+
+const node6 = node3.add('6');
+const node7 = node3.add('7');
+
+/* level 4 */
+const node8 = node4.add('8');
+const node9 = node4.add('9');
+const node10 = node4.add('10');
+
+const node11 = node5.add('11');
+const node12 = node5.add('12');
+
+const node13 = node6.add('13');
+const node14 = node6.add('14');
+const node15 = node7.add('15');
+
+
+/* tree.traverseBF(node => console.log(node.data)); */
+/* tree.traverseBFFromNode(node => console.log(node.data), node2); */
+/* tree.traverseDF(node => console.log(node.data)); */
+tree.traverseDFFromNode(node => console.log(node.data), node2);
+
+
 
 module.exports = { Tree, Node };
