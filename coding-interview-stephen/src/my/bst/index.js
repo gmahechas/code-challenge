ff924916b30092f1bdb04722c5b5f0e25dd12b82
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-08-13 21:26:58 -0500

    my-2

--- coding-interview-stephen/src/my/bst/index.js
+++ coding-interview-stephen/src/my/bst/index.js
@@ -11,6 +11,53 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
 
+	constructor(data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+
+	insert(data) {
+		if (data < this.data && this.left) {
+			debugger;
+			this.left.insert(data);
+		} else if (data < this.data) {
+			debugger;
+			this.left = new Node(data);
+		} else if (data > this.data && this.right) {
+			this.right.insert(data);
+		} else {
+			this.right = new Node(data);
+		}
+	}
+
+	contanis(data) {
+		if(this.data == data) {
+			return this;
+		}
+
+		if(this.data < data && this.right) {
+			return this.right.contanis(data);
+		} else if (this.data > data && this.left) {
+			return this.left.contanis(data);
+		}
+
+		return null;
+	}
+}
+
+function solution() {
+	const node = new Node(10);
+	node.insert(5);
+	node.insert(15);
+	node.insert(20);
+	node.insert(0);
+	node.insert(-5);
+	node.insert(3);
+	console.log(node.contanis(3));
+}
+
+solution();
 module.exports = Node;
